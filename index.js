4f73b4e0bd142fd840e8c80a1efb9a4296f90ac7
Patrick Seal <code@plasticrake.com>
2017-09-05 21:51:40 -0700

    use api

--- index.js
+++ index.js
@@ -4,6 +4,5 @@ const Hs100Platform = require('./lib/platform');
 
 module.exports = function (homebridge) {
   const dynamic = true;
-  Hs100Platform.setHomebridge(homebridge);
   homebridge.registerPlatform('homebridge-hs100', 'Hs100', Hs100Platform, dynamic);
 };
