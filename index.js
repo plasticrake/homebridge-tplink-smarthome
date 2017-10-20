dde6f53e607421f119befc7f554efea288f154a0
Patrick Seal <code@plasticrake.com>
2017-10-19 19:56:25 -0700

    Rename to homebridge-tplink-smarthome (#37)
    * rename homebridge-hs100 to homebridge-tplink-smarthome

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const Hs100Platform = require('./lib/platform');
+const TplinkSmarthomePlatform = require('./lib/platform');
 
 module.exports = function (homebridge) {
   const dynamic = true;
-  homebridge.registerPlatform('homebridge-hs100', 'Hs100', Hs100Platform, dynamic);
+  homebridge.registerPlatform('homebridge-tplink-smarthome', 'TplinkSmarthome', TplinkSmarthomePlatform, dynamic);
 };
