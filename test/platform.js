c66538a1353e2ea5e19668f0bada563d70b54292
plasticrake <code@plasticrake.com>
2020-05-12 20:02:22 -0700

    test: add TplinkAccessory test

--- test/platform.js
+++ test/platform.js
@@ -1,10 +1,12 @@
 /* eslint-disable no-underscore-dangle */
+const EventEmitter = require('events');
 const { expect } = require('chai');
 const rewire = require('rewire');
 
 const { API } = require('homebridge/lib/api');
 
 const TplinkSmarthomePlatform = rewire('../lib/platform');
+const { TplinkAccessory } = require('../lib/tplink-accessory');
 const {
   platformAccessories,
   platformAccessoriesIssues,
@@ -20,8 +22,18 @@ const log = {
 
 describe('TplinkSmarthomePlatform', function () {
   let platform;
+  let tplinkDevice;
   beforeEach(function () {
     platform = new TplinkSmarthomePlatform(log, {}, new API());
+
+    tplinkDevice = new EventEmitter();
+    Object.assign(tplinkDevice, {
+      id: 'ABC',
+      deviceType: 'plug',
+      model: 'HS100',
+      supportsDimmer: false,
+      alias: 'TEST',
+    });
   });
 
   describe('~createTplinkAccessory', function () {
@@ -29,13 +41,14 @@ describe('TplinkSmarthomePlatform', function () {
       'createTplinkAccessory'
     );
     // const platform = { config: { switchModels: ['HS200'] } };
-    it('should add platformAccessory to #homebridgeAccessories', function () {
-      createTplinkAccessory(platform);
+    it('should create TplinkAccessory', function () {
+      const accessory = createTplinkAccessory(platform, null, tplinkDevice);
+      expect(accessory).to.be.instanceof(TplinkAccessory);
     });
   });
 
   describe('#addAccessory', function () {
-    it('should add platformAccessory to #homebridgeAccessories', function () {});
+    it.skip('should add platformAccessory to #homebridgeAccessories', function () {});
   });
 
   describe('#configureAccessory', function () {
