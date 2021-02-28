4796042a3deebbd4d7e0fbd1fc69e259c2a131c8
Patrick Seal <code@plasticrake.com>
2021-02-28 10:18:23 -0800

    style: prettier config changes from upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,6 @@ module.exports = {
         'airbnb-typescript/base',
         'plugin:@typescript-eslint/eslint-recommended',
         'plugin:@typescript-eslint/recommended',
-        'prettier/@typescript-eslint',
         'plugin:prettier/recommended',
       ],
       rules: {
