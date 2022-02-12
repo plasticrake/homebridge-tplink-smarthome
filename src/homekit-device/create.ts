68e13dea462679c5c61c12e3b09de59b0e08120d
Patrick Seal <code@plasticrake.com>
2022-02-12 09:42:01 -0800

    refactor: major refactor, removed unneeded abstraction

--- src/homekit-device/create.ts
+++ src/homekit-device/create.ts
@@ -1,22 +1,37 @@
-import type { Categories } from 'homebridge';
+import type { PlatformAccessory } from 'homebridge';
 
 import type TplinkSmarthomePlatform from '../platform';
+import type { TplinkSmarthomeAccessoryContext } from '../platform';
 import type { TplinkDevice } from '../utils';
 
-import HomeKitDevice from '.';
+import HomekitDevice from '.';
 import HomeKitDeviceBulb from './bulb';
 import HomeKitDevicePlug from './plug';
+import { TplinkSmarthomeConfig } from '../config';
 
 /**
  * Factory method to create a HomeKitDeviceBulb or HomeKitDevicePlug.
  */
 export default function create(
   platform: TplinkSmarthomePlatform,
-  tplinkDevice: TplinkDevice,
-  category: Categories
-): HomeKitDevice {
+  config: TplinkSmarthomeConfig,
+  homebridgeAccessory:
+    | PlatformAccessory<TplinkSmarthomeAccessoryContext>
+    | undefined,
+  tplinkDevice: TplinkDevice
+): HomekitDevice {
   if (tplinkDevice.deviceType === 'bulb') {
-    return new HomeKitDeviceBulb(platform, tplinkDevice, category);
+    return new HomeKitDeviceBulb(
+      platform,
+      config,
+      homebridgeAccessory,
+      tplinkDevice
+    );
   }
-  return new HomeKitDevicePlug(platform, tplinkDevice, category);
+  return new HomeKitDevicePlug(
+    platform,
+    config,
+    homebridgeAccessory,
+    tplinkDevice
+  );
 }
