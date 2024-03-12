db5e103b8d77a79646b89f7b879169d4b08a3fac
ZeliardM <ZeliardM@gmail.com>
2024-03-12 10:00:36 -0400

    pulling latest sysinfo to make sure powerState is updated correctly

--- src/homekit-device/powerstrip.ts
+++ src/homekit-device/powerstrip.ts
@@ -112,8 +112,9 @@ export default class HomeKitDevicePowerStrip extends HomekitDevice {
 
     let oldChildState = childDevice.state;
 
-    this.tplinkDevice.on('power-update', () => {
-      const newChildState = this.tplinkDevice.sysInfo.children?.find(childDevice => childDevice.id === childDevice.id)?.state;
+    this.tplinkDevice.on('power-update', async () => {
+      const sysInfo = await this.tplinkDevice.getSysInfo();
+      const newChildState = sysInfo.children?.find(childDevice => childDevice.id === childDevice.id)?.state;
       
       if (newChildState) {
         if (newChildState !== oldChildState) {
